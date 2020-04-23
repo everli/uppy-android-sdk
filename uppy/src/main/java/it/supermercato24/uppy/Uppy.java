c3882891f1a9bf1cc97f40ff508061bc0c5e6cf3
jibbo <giovanni.defrancesco@supermercato24.it>
2020-04-23 11:11:08 +0200

    started to create the skeleton of the app

--- uppy/src/main/java/it/supermercato24/uppy/Uppy.java
+++ uppy/src/main/java/it/supermercato24/uppy/Uppy.java
@@ -1,28 +1,42 @@
 package it.supermercato24.uppy;
 
+import retrofit2.Retrofit;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Uppy {
 
-    private static volatile Uppy INSTANCE;
+    private static volatile Uppy instance;
 
     private final String serverUrl;
-    private final String appToken;
 
-    private Uppy(String serverUrl, String appToken) {
+    private final Retrofit retrofit;
+    private final UppyService uppyService;
+
+    private Uppy(String serverUrl) {
         this.serverUrl = serverUrl;
-        this.appToken = appToken;
-        INSTANCE = this;
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(serverUrl)
+                .build();
+
+        uppyService = retrofit.create(UppyService.class);
+
+        instance = this;
     }
 
-    public synchronized Uppy getInstance(String serverUrl, String appToken) {
-        if (INSTANCE == null) {
+    public static synchronized void init(String serverUrl) {
+        if (instance == null) {
             synchronized (Uppy.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = new Uppy(serverUrl, appToken);
+                if (instance == null) {
+                    instance = new Uppy(serverUrl);
                 }
             }
         }
-        return INSTANCE;
     }
 
-    public
+    public static void isLatestVersion() {
+        throw new NotImplementedException();
+        // TODO
+        // instance.uppyService.checkLatestVersion("", null);
+    }
 }
